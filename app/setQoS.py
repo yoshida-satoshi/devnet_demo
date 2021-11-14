7f7fc304cde6881549dac16c0ee5e9fd2ec60a25
Azure_Workstation_Windows1 <Azure_Workstation_Windows1@devnet.traino.cat>
2021-11-14 23:57:31 +0900

    sh run

--- app/setQoS.py
+++ app/setQoS.py
@@ -74,3 +74,4 @@ print(response.text)
 
 
 
+
---
+++ app/show_run.py
@@ -0,0 +1,12 @@
+# Webex Teamsにshow run送信
+import requests
+import cli
+showrun = cli.execute("shwo run int gi1")
+
+payload="{\r\n  \"roomId\": \"Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vNzBlNWYwZTAtNDQ5Yi0xMWVjLTkxMzEtMWRlMmQ0OThhM2Ey\",\r\n  \"text\": \"{}\"\r\n}".format(showrun)
+headers = {
+  'Content-Type': 'application/json',
+  'Authorization': 'Bearer MTE0Njc4OTktYThlMi00MTEwLWIxNmMtZGUzNWEzNGM1MjM1YzhmMWJlMTMtMDYw_P0A1_c98415b4-c72b-47aa-82ca-ffe96054cf5a'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)

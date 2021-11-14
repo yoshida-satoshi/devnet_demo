d6f2fa6d79e6208e02f2673fbb47ad1c09b98343
Azure_Workstation_Windows1 <Azure_Workstation_Windows1@devnet.traino.cat>
2021-11-15 00:16:57 +0900

    cli

--- app/show_run.py
+++ app/show_run.py
@@ -1,13 +1,18 @@
 # Webex Teams show run
 import requests
 import json
+
+
 import cli
+
 showrun = cli.execute("show run int gi1")
 
-payload={"roomId":"Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vNzBlNWYwZTAtNDQ5Yi0xMWVjLTkxMzEtMWRlMmQ0OThhM2Ey","text":showrun.text)
+payload={"roomId":"Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vNzBlNWYwZTAtNDQ5Yi0xMWVjLTkxMzEtMWRlMmQ0OThhM2Ey","text":"{}".format(showrun)}
 headers = {
   'Content-Type': 'application/json',
   'Authorization': 'Bearer MTE0Njc4OTktYThlMi00MTEwLWIxNmMtZGUzNWEzNGM1MjM1YzhmMWJlMTMtMDYw_P0A1_c98415b4-c72b-47aa-82ca-ffe96054cf5a'
 }
+
+url = "https://webexapis.com/v1/messages"
 
 response = requests.request("POST", url, headers=headers, data=json.dumps(payload))

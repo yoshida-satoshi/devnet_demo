903aae35f39695b2e453354697202be1632c4ef7
Azure_Workstation_Windows1 <Azure_Workstation_Windows1@devnet.traino.cat>
2021-11-15 13:34:51 +0900

    fix1

--- app/show_run.py
+++ app/show_run.py
@@ -8,6 +8,7 @@ import cli
 showrun = cli.execute("show run int gi1")
 
 payload={"roomId":"Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vNzBlNWYwZTAtNDQ5Yi0xMWVjLTkxMzEtMWRlMmQ0OThhM2Ey","text":"{}".format(showrun)}
+
 headers = {
   'Content-Type': 'application/json',
   'Authorization': 'Bearer MTE0Njc4OTktYThlMi00MTEwLWIxNmMtZGUzNWEzNGM1MjM1YzhmMWJlMTMtMDYw_P0A1_c98415b4-c72b-47aa-82ca-ffe96054cf5a'

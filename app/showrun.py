8dab5146d4213c08607419a7ec4054411a1f5cfd
Azure_Workstation_Windows1 <Azure_Workstation_Windows1@devnet.traino.cat>
2021-11-16 13:06:15 +0900

    s

--- app/showrun.py
+++ app/showrun.py
@@ -4,15 +4,17 @@ import cli
 show_run = cli.execute("show run int gi1")
 
 import requests
+import json
 
 url = "https://webexapis.com/v1/messages"
 
-payload="{\r\n  \"roomId\": \"Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vZjFlMjVkYzAtNDY4ZC0xMWVjLTg1YTAtMmI1Y2QxMzA4YTY5\",\r\n  \"text\": \"" + show_run + "\"\r\n}"
+payload={"roomId":"Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vZjFlMjVkYzAtNDY4ZC0xMWVjLTg1YTAtMmI1Y2QxMzA4YTY5","text":"{}".format(show_run)}
+
 headers = {
   'Content-Type': 'application/json',
   'Authorization': 'Bearer NGUxYmQ4ZjYtNDAxMi00MTY1LTk1MTQtOTJmYjk4OWY2NDExZDAzMThlZDctMDdl_P0A1_c98415b4-c72b-47aa-82ca-ffe96054cf5a'
 }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
 
 print(response.text)

839e9ee47d49170a19961f4b21e856103abb7c31
Azure_Workstation_Windows1 <Azure_Workstation_Windows1@devnet.traino.cat>
2021-12-06 15:44:22 +0900

    DevNet Demo準備

--- app/showrun.py
+++ app/showrun.py
@@ -8,11 +8,11 @@ import json
 
 url = "https://webexapis.com/v1/messages"
 
-payload={"roomId":"Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vZjFlMjVkYzAtNDY4ZC0xMWVjLTg1YTAtMmI1Y2QxMzA4YTY5","text":show_run}
+payload={"roomId":"Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vOTM4MjI1NTAtNTY1ZC0xMWVjLTk3MjQtMGI2N2JmNjg5MjEy","text":show_run}
 
 headers = {
   'Content-Type': 'application/json',
-  'Authorization': 'Bearer NGUxYmQ4ZjYtNDAxMi00MTY1LTk1MTQtOTJmYjk4OWY2NDExZDAzMThlZDctMDdl_P0A1_c98415b4-c72b-47aa-82ca-ffe96054cf5a'
+  'Authorization': 'Bearer MWJlNzg3YTYtZTQyNy00YmQ0LThhMzgtYjRjMmJhZWFmZDhjZTI2MjA4ODgtNmU0_P0A1_c98415b4-c72b-47aa-82ca-ffe96054cf5a'
 }
 
 response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
